2fa6f7b33f6d7887ad4a78063c36146f7e76aa53
Callum Laing <callum.laing91@hotmail.co.uk>
2023-09-27 22:29:29 +0100

    changed to hardcoded html

--- app.js
+++ app.js
@@ -1,31 +1,38 @@
-const app = document.querySelector("#app");
 const cityInput = document.querySelector("#cityInput");
 const searchButton = document.querySelector("#searchButton");
+const weatherCard = document.querySelector("#weatherCard");
+const locationName = document.querySelector("#locationName");
+const temperature = document.querySelector("#temperature");
+var myKey = config.MY_KEY;
 
-searchButton.addEventListener("click", function () {
+searchButton.addEventListener("click", async function () {
   const city = cityInput.value;
   if (city) {
-    fetchWeather(city);
+    try {
+      const response = await fetch(
+        `http://api.weatherapi.com/v1/current.json?key=${myKey}&q=${city}&aqi=no`,
+        {
+          mode: "cors",
+        }
+      );
+      console.log(response);
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! Status: ${response.status}`);
+      }
+
+      const data = await response.json();
+
+      locationName.textContent = data.location.name;
+      temperature.textContent = `${data.current.temp_c}°C`;
+      weatherCard.style.display = "block";
+    } catch (error) {
+      console.error("Error fetching weather data:", error);
+      alert("An error occurred while fetching weather data.");
+      weatherCard.style.display = "none";
+    }
   } else {
     alert("Please enter a city name.");
+    weatherCard.style.display = "none";
   }
 });
-
-function fetchWeather(city) {
-  fetch(
-    `http://api.weatherapi.com/v1/current.json?key=2294d5cf68954095a5b221927231809&q=${city}&aqi=no`,
-    {
-      mode: "cors",
-    }
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (response) {
-      app.innerHTML = `Location: ${response.location.name}Temperature: ${response.current.temp_c}`;
-    })
-    .catch(function (error) {
-      console.error("Error fetching weather data:", error);
-      app.textContent = "An error occurred while fetching weather data.";
-    });
-}
